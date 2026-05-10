1e5e752fb40e7407c8499fe52532fce728e84436
Pedro Acuña Talavera <josueacut13@gmail.com>
2026-05-09 21:50:06 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,36 @@
+const CACHE_NAME = 'irex-app-v2'; // Cambiamos el nombre para forzar la actualización
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
+
+// Instalación: Guardamos solo lo básico
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('upc-a-cache-v1').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './manifest.json',
-                './sw.js',
-                './icon.png',
-                './icon-512.png'
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
+// Activación: Borramos cachés viejos del repositorio anterior
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Estrategia: Buscar en internet primero, si falla, usar caché
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
 });
